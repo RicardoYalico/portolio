b0c9bee19e9dc3e2abf6d8534a8a059eba5b245d
Ricardo Anderson Yalico Roncal <u201920140@upc.edu.pe>
2026-01-26 15:54:40 -0500

    demo

--- src/config/postmark.js
+++ src/config/postmark.js
@@ -1,6 +1,6 @@
-var postmark = require('postmark');
+/* var postmark = require('postmark');
 
 // Send an email:
 var client = new postmark.ServerClient(process.env.POSTMARK_SERVER_CLIENT);
 
-export default client;
+export default client; */
